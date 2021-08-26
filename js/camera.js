8fc5ea6c4fc19a025f3c135bb1a8bc2309416754
felipehenriquee <79702834+felipehenriquee@users.noreply.github.com>
2021-08-26 14:29:37 -0300

    Update camera.js

--- js/camera.js
+++ js/camera.js
@@ -89,7 +89,7 @@
       videoStream = await navigator.mediaDevices.getUserMedia(constraints);
       video.srcObject = videoStream;
     } catch (err) {
-      alert("Could not access the camera");
+      alert(err;
     }
   }
 
