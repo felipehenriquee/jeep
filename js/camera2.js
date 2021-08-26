584cb2a22917c1cc495bfd6a69f368799bb68c95
felipehenriquee <79702834+felipehenriquee@users.noreply.github.com>
2021-08-26 17:28:11 -0300

    Create camera2.js

---
+++ js/camera2.js
@@ -0,0 +1,70 @@
+(function() {
+    if (
+      !"mediaDevices" in navigator ||
+      !"getUserMedia" in navigator.mediaDevices
+    ) {
+      alert("Câmera não está disponível nesse navegador");
+      return;
+    }
+  
+    // get page elements
+    const video = document.querySelector("#video");
+    const btnScreenshot = document.querySelector("#btnScreenshot");
+    const canvas = document.querySelector("#canvas");
+    
+  
+    // video constraints
+    const constraints = {
+      video: {
+        width: {
+          min: 1280,
+          ideal: 1920,
+          max: 2560,
+        },
+        height: {
+          min: 720,
+          ideal: 1080,
+          max: 1440,
+        },
+      },
+    };
+  
+    // use front face camera
+    let useFrontCamera = false;
+  
+    // current video stream
+    let videoStream;
+  
+    
+    btnScreenshot.addEventListener("click", function () {
+        const img = document.createElement("img");
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        canvas.getContext("2d").drawImage(video, 0, 0);
+        img.src = canvas.toDataURL("image/png");
+        screenshotsContainer.prepend(img);
+      });
+    // stop video stream
+    function stopVideoStream() {
+      if (videoStream) {
+        videoStream.getTracks().forEach((track) => {
+          track.stop();
+        });
+      }
+    }
+  
+    // initialize
+    async function initializeCamera() {
+      stopVideoStream();
+      constraints.video.facingMode = useFrontCamera ? "user" : "environment";
+  
+      try {
+        videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+        video.srcObject = videoStream;
+      } catch (err) {
+        alert("A Câmera não pode ser acessada");
+      }
+    }
+  
+    initializeCamera();
+  })();
